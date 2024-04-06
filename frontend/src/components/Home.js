8369e5c50052e74266ab91c87566e05649f8eb60
Vishmitha Nipun <it22336354@my.sliit.lk>
2024-04-06 21:41:38 +0530

    Update Home.js

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style/main.css";
+import "./style/Home.css";
+
+
+export default function Home(){
+    return(
+        <div className="img1">
+            <img src="../../Images/read and black.jpg" alt="Read and Black"  width="1872" height="850"  />
+            <h1>Hello</h1>
+        </div>
+    );
+}
