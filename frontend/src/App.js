d9173650608dc291d70abbbf18b3a36a70d1d107
Vishmitha Nipun <it22336354@my.sliit.lk>
2024-04-06 11:56:28 +0530

    app css and js change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+//import AddMedia from "./components/AddMedia";
+import gallery from "./components/Gallery";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Helloooooooooooooooooo</h1>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/gallery" Component={gallery} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
