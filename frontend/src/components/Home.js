c7373b8a85dec629a3a1844a5fc8474fc4121f7d
Vishmitha Nipun <it22336354@my.sliit.lk>
2024-04-08 14:11:05 +0530

    update home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -20,10 +20,16 @@ export default function Home(){
                 <h1 className="add2">Rrajakeeya Mawatha,</h1>
                 <h1 className="add3">Collombo</h1>
             </div>
+
+            <img src="../../Images/reg.jpg" alt="Read and Black"  width="1857" height="950"  />
             
             <div className="overlay3">
+            <h1 className="btob">B TO B</h1>    
             <h1 className="regInfo">Registration Information</h1>
-                
+            <p className="ph">Registration opens on June 3, 2024 at 12:00am . SPOTS ARE LIMITED...BE SURE TO REGISTER EARLY!</p> 
+            <p className="ph">Registration will be accepted on a first-come, first served basis.Spots are limited in each division.  Once a division is full, no more entries will be accepted for that division</p>  
+            <p className="ph">Payment must be made in FULL in order for registration to be processed.</p> 
+            <button type="button" class="btn btn-primary btn-lg custom-btn">More Information</button>
             </div>
 
         </div>
